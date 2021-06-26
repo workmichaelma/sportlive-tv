ee88f29033d62c3bf36141cb24a6cd7e7338d1bf
Michael Ma <work.michael.ma@gmail.com>
2021-06-27 01:57:56 +0800

    Update match frame to use url

--- Player.js
+++ Player.js
@@ -1,21 +1,17 @@
-import React, { useState, useEffect, useMemo } from 'react'
-import { StyleSheet, Text, View, Modal } from 'react-native';
-import { Avatar, Button, Chip, } from 'react-native-paper'
+import React, { useState, useEffect, useMemo } from "react";
+import { StyleSheet, Text, View, Modal } from "react-native";
+import { Avatar, Button, Chip } from "react-native-paper";
 import { Dimensions } from "react-native";
-import { get } from 'lodash'
-import Iframe from './Iframe'
+import { get } from "lodash";
+import Iframe from "./Iframe";
 
-var width = Dimensions.get('window').width; //full width
-var height = Dimensions.get('window').height - 30; //full height
-
-const getUrl = (id, subid) => {
-  return `https://www.heibaizhibo.com/play-iframe/${id}#${subid}`
-}
+var width = Dimensions.get("window").width; //full width
+var height = Dimensions.get("window").height - 30; //full height
 
 var styles = StyleSheet.create({
   closeBtn: {
-    backgroundColor: 'transparent',
-    color: 'black',
+    backgroundColor: "transparent",
+    color: "black",
     width: 160,
     height: 30,
     fontSize: 20,
@@ -23,39 +19,34 @@ var styles = StyleSheet.create({
 });
 
 const Player = ({ player, hidePlayer }) => {
-  const { match, visible } = player
-  const [source, setSource] = useState({})
-
-  useEffect(() => {
-    const firstSource = get(match, 'source[0]', {})
-    setSource(firstSource)
-  }, [match])
+  const { match, visible } = player;
 
   const url = useMemo(() => {
-    if (match.id && source.id) {
-      return getUrl(match.id, source.id)
-    } else {
-      return false
-    }
-  }, [match, source])
+    return get(match, "url", false);
+  }, [match]);
 
   return (
     <Modal visible={visible} style={{ flex: 1 }}>
       <View style={{ flex: 1, width, height }}>
-        {
-          url && <Iframe url={url} style={{ width, height }} />
-        }
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <Button style={styles.closeBtn} icon="arrow-left-circle" onPress={() => { hidePlayer() }}>
+        {url && <Iframe url={url} style={{ width, height }} />}
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Button
+            style={styles.closeBtn}
+            icon="arrow-left-circle"
+            onPress={() => {
+              hidePlayer();
+            }}
+          >
             返回
           </Button>
-          <Text style={{ textAlign: 'center', marginLeft: 40 }}>
-            首次進來後，按一下 <Avatar.Icon size={13} icon="arrow-down-thick" /> 及 <Avatar.Icon size={13} icon="bullseye" /> 以解除靜音
+          <Text style={{ textAlign: "center", marginLeft: 40 }}>
+            首次進來後，按一下 <Avatar.Icon size={13} icon="arrow-down-thick" />{" "}
+            及 <Avatar.Icon size={13} icon="bullseye" /> 以解除靜音
           </Text>
         </View>
       </View>
-    </Modal >
+    </Modal>
   );
-}
+};
 
-export default Player
+export default Player;
